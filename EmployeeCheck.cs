c6c1a3524d86f1462ebb2c15a8a0cdacb9a8c786
Ashuakhira <bashok592@gmail.com>
2023-04-26 08:46:15 +0530

    Attendance Branch is Added

---
+++ EmployeeCheck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class EmployeeCheck
+    {
+        public static void CheckAttendance()
+        {
+            int IS_FULL_TIME = 1;
+            Random rdm = new Random();
+            int empchk = rdm.Next(2);
+            if(empchk==IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee Is Absent");
+            }
+        }
+    }
+}
